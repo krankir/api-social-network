fbce7ed2bb966cd2f356371ecd95702936188ae3
Анатолий Редько <stroitel.proekt@gmail.com>
2022-10-20 18:19:44 +0500

    dobavil modeli

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,6 +4,25 @@ from django.db import models
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=200,
+                             verbose_name='Жанр',
+                             help_text='Укажите жанр')
+    slug = models.SlugField(max_length=255,
+                            unique=True,
+                            verbose_name='Параметр',
+                            help_text='Адрес')
+    description = models.TextField(verbose_name='Содержание',
+                                   help_text='Содержание')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
@@ -24,3 +43,23 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE,
+                             related_name='follower',
+                             verbose_name='Подписчик')
+    following = models.ForeignKey(User,
+                                  on_delete=models.CASCADE,
+                                  related_name='following',
+                                  verbose_name='Подписка')
+
+    def __str__(self):
+        return f'{self.user} follows {self.following}'
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = [models.UniqueConstraint(fields=['user', 'following'],
+                                               name='unique_follow')]
